83835bf88c3a1b297b52a8c8a270dd6a98c2637f
lackson <93818145+lacksonz@users.noreply.github.com>
2025-08-12 19:40:55 +0100

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,20 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Welcome to my stuff</h1>
+      <div className="button-group">
+        <a href="#about" className="btn">
+          🧑‍💻 About Me
+        </a>
+        <a href="#json" className="btn">
+          🧾 JSON Designer
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
